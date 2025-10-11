9b458befbc8b1be407018875961dc9784da4cdcb
Epres Tamás <104941550+Eper00@users.noreply.github.com>
2025-10-11 22:52:50 +0200

    Delete MPC directory

--- MPC/test_site.py
+++
@@ -1,8 +0,0 @@
-from control import controller,control_loop
-from parameters import *
-from support import save_data,visualize_results,simulate,simualtion_verdict
-
-
-w_opt=control_loop(3,x0_val,x0_init,theta_dot_ref,N,M)
-#simualtion_verdict(x0_val,2.5,0.001,0.05)
-
